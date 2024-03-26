ffbb2a2b1b2b26919b46ecc8894e3e6880d062c0
Layne Chant <layne.chant@gmail.com>
2024-03-26 13:06:58 -0400

    Fixed Player Health Bar in battle screen

--- EN-JP-LC-GridGame/Assets/Scripts/Player/PlayerStats.cs
+++ EN-JP-LC-GridGame/Assets/Scripts/Player/PlayerStats.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public int playerLevel = 1;
-    public int playerHealth = 15;
+    public int playerHealth = 21;
     public int playerMaxHealth = 17;
 
     [SerializeField] private TextMeshProUGUI maxHealthText;
